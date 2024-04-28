cea8f4305256a0b47afacb8de5ba475ad620af5c
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2024-04-28 19:49:59 +0300

    spring boot course overview

---
+++ 01-spring-boot-overview/01-my-spring-boot-demo/src/test/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestControllerTest.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunRestControllerTest {
+
+    @Test
+    void hello() {
+    }
+}

142ca0c1dfc25c2e93d683c8dfd213fe54576e38
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2024-04-29 16:07:44 +0300

    Qualifier used to which one implement when we have a multiImplement for interface

---
+++ 02-spring-boot-core/04-qualifires/src/main/java/com/luv2code/springcoredemo/rest/CoachController.java
@@ -0,0 +1,22 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.coach.CoachInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController {
+    final private  CoachInterface myCoach;
+
+    @Autowired
+    public  CoachController( @Qualifier("basketballCoach")  CoachInterface myCoach){
+        this.myCoach=myCoach;
+    }
+
+    @GetMapping("/daily-workout")
+    public  String getDailyWorkout(){
+        return  this.myCoach.goDailyWorkout();
+    }
+}

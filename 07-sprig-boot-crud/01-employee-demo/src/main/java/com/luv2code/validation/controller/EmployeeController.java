2f50d1b4a94ef84a0be180b2aaa2942d58497ef9
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2024-05-10 20:39:23 +0300

    spring boot employee crud example  with dtos for validation

---
+++ 07-sprig-boot-crud/01-employee-demo/src/main/java/com/luv2code/validation/controller/EmployeeController.java
@@ -0,0 +1,56 @@
+package com.luv2code.validation.controller;
+
+import com.luv2code.validation.dtos.EmployeeDTO;
+import com.luv2code.validation.entity.Employee;
+import com.luv2code.validation.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class EmployeeController {
+    private  final EmployeeService employeeService;
+    @Autowired
+    public EmployeeController(@Qualifier("employeeServiceImpl") EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public  String getAllEmployee(Model uiModel){
+        List<Employee> employees = employeeService.findAllByOrderByIdDesc();
+        uiModel.addAttribute("employees", employees);
+        return "employee-list";
+    }
+    @GetMapping("/employees/{id}")
+    public  String getEmployeeById(@PathVariable("id") long id,Model uiModel ){
+        Employee employee = employeeService.getById(id);
+        uiModel.addAttribute("employee", employee);
+
+        return "show";
+    }
+    @GetMapping("/employees/new")
+    public  String showEmployeeForm(Model uiModel){
+            uiModel.addAttribute("employee",new EmployeeDTO());
+            return  "create";
+    }
+    @PostMapping("/employees")
+    public String createEmployee(@Valid @ModelAttribute("employee") EmployeeDTO employeeDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            return "create";
+        }
+
+        employeeService.create(employeeDTO);
+        return "redirect:/";
+    }
+    @GetMapping("/employees/{id}/delete")
+    public  String deleteEmployee(@PathVariable("id") long id ){
+       employeeService.delete(id);
+        return "redirect:/";
+    }
+}
---
+++ 07-sprig-boot-crud/01-employee-demo/src/main/java/com/luv2code/validation/service/EmployeeServiceImpl.java
@@ -0,0 +1,61 @@
+package com.luv2code.validation.service;
+
+import com.luv2code.validation.doa.EmployeeRepository;
+import com.luv2code.validation.dtos.EmployeeDTO;
+import com.luv2code.validation.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class EmployeeServiceImpl implements  EmployeeService {
+      private final  EmployeeRepository employeeRepository;
+    @Autowired
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return this.employeeRepository.findAll();
+    }
+
+    @Override
+    public List<Employee> findAllByOrderByIdDesc() {
+        return this.employeeRepository.findAllByOrderByIdDesc();
+    }
+
+    @Override
+    public Employee getById(long id) {
+        return this.employeeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid employee ID: " + id));
+
+    }
+    @Transactional
+    @Override
+    public Employee create(EmployeeDTO employeeDTO) {
+        Employee employee =new Employee();
+        employee.setFirstName(employeeDTO.getFirstName());
+        employee.setLastName(employeeDTO.getLastName());
+        employee.setEmail(employeeDTO.getEmail());
+
+        return this.employeeRepository.save(employee);
+    }
+    @Transactional
+    @Override
+    public Employee update(long id, EmployeeDTO employeeDTO) {
+        Employee employee = getById(id);
+        employee.setFirstName(employeeDTO.getFirstName());
+        employee.setLastName(employeeDTO.getLastName());
+        employee.setEmail(employeeDTO.getEmail());
+        return employeeRepository.save(employee);
+
+    }
+    @Transactional
+    @Override
+    public void delete(long id) {
+        Employee employee = getById(id);
+        employeeRepository.delete(employee);
+    }
+}
